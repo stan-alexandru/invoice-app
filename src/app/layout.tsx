67c804c0bb63a1b4f888dc4a4ad30dc4a6aa674c
stan-alexandru <stan.alexandru26@gmail.com>
2024-06-11 04:30:47 +0300

    feat:add assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<head>
+				<link rel='icon' href='/favicon-32x32.png' sizes='any' />
+			</head>
 			<body>{children}</body>
 		</html>
 	);
